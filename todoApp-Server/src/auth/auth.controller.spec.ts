213e61bd60c3c94dae6e2dc592aac980574c6f62
kagan <kagancancubukcu@gmail.com>
2024-09-09 16:29:24 +0300

    Fixed test errors

--- todoApp-Server/src/auth/auth.controller.spec.ts
+++ todoApp-Server/src/auth/auth.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { JwtService } from '@nestjs/jwt';
+import { getModelToken } from '@nestjs/mongoose';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +10,14 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        JwtService,
+        {
+          provide: getModelToken('User'),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
