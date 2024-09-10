5815679fd19d13d7f43cc7c1bb93fdffbbeb0733
kagan <kagancancubukcu@gmail.com>
2024-09-10 12:16:14 +0300

    Added global validation pipe to main.ts

--- todoApp-Server/src/main.ts
+++ todoApp-Server/src/main.ts
@@ -2,9 +2,11 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
