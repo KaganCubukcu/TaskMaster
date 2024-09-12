c965aabef999329f27d335fa90a9058ae03aa3ab
kagan <kagancancubukcu@gmail.com>
2024-09-12 13:47:13 +0300

    Added auth service

--- todoApp-Server/src/auth/auth.controller.ts
+++ todoApp-Server/src/auth/auth.controller.ts
@@ -1,40 +1,30 @@
-import {
-  Controller,
-  Post,
-  Body,
-  UnauthorizedException,
-  BadRequestException,
-  InternalServerErrorException,
-  UseGuards,
-} from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { LoginDto } from './DTOs/login.dto';
-import { RegisterDto } from './DTOs/register.dto';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import {Controller, Post, Body, UnauthorizedException, BadRequestException, InternalServerErrorException} from '@nestjs/common'
+import {AuthService} from './auth.service'
+import {LoginDto} from './DTOs/login.dto'
+import {RegisterDto} from './DTOs/register.dto'
 
 @Controller('auth')
-@UseGuards(JwtAuthGuard)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
-    const user = await this.authService.validateUser(loginDto.email, loginDto.password);
+    const user = await this.authService.validateUser(loginDto.email, loginDto.password)
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException()
     }
-    return this.authService.login(user);
+    return this.authService.login(user)
   }
 
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
     try {
-      return await this.authService.register(registerDto);
+      return await this.authService.register(registerDto)
     } catch (error) {
       if (error.code === 11000) {
-        throw new BadRequestException('Email already exists');
+        throw new BadRequestException('Email already exists')
       }
-      throw new InternalServerErrorException();
+      throw new InternalServerErrorException()
     }
   }
 }
