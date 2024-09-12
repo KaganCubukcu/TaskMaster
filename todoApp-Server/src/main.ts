42aead6aca6a28bd0bd90ae1931cecb68fa8a121
kagan <kagancancubukcu@gmail.com>
2024-09-12 13:45:16 +0300

    Added environments and enhanced CORS in server

--- todoApp-Server/src/main.ts
+++ todoApp-Server/src/main.ts
@@ -1,17 +1,23 @@
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import {Logger} from '@nestjs/common'
+import {NestFactory} from '@nestjs/core'
 
-import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import {AppModule} from './app/app.module'
+import {ValidationPipe} from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  })
+  app.useGlobalPipes(new ValidationPipe())
+  const globalPrefix = 'api'
+  app.setGlobalPrefix(globalPrefix)
+  const port = process.env.PORT || 3000
+  await app.listen(port)
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`)
 }
 
-bootstrap();
+bootstrap()
