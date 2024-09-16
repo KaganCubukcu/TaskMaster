254260f3dfc1f9af434f44fa123ce6cfaefd55d7
kagan <kagancancubukcu@gmail.com>
2024-09-16 14:04:22 +0300

    Added todosearch component

---
+++ todoApp-Client/src/app/components/todo/todoSearch/todosearch.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+import {CommonModule} from '@angular/common'
+
+@Component({
+  selector: 'app-todosearch',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todosearch.component.html',
+  styleUrl: './todosearch.component.css'
+})
+export class TodosearchComponent {}
