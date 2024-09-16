f699d1a397095f0320fe44bca147f9a0c9a847a0
kagan <kagancancubukcu@gmail.com>
2024-09-16 14:02:36 +0300

    Added TodoForm into Main Layout

--- todoApp-Client/src/app/app.component.ts
+++ todoApp-Client/src/app/app.component.ts
@@ -2,10 +2,12 @@ import {Component} from '@angular/core'
 import {RouterModule} from '@angular/router'
 import {HeaderComponent} from './components/shared/header/header.component'
 import {FooterComponent} from './components/shared/footer/footer.component'
+import {TodoFormComponent} from './components/todo/todoForm/todoform.component'
+import {CommonModule} from '@angular/common'
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent, TodoFormComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
