f89a56eb93e907c8deb4119da740694a04221936
kagan <kagancancubukcu@gmail.com>
2024-09-16 14:02:00 +0300

    Redirected root route to Home page instead of Register page

--- todoApp-Client/src/app/app.routes.ts
+++ todoApp-Client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import {Route} from '@angular/router'
 import {LoginComponent} from './components/auth/login/login.component'
 import {RegisterComponent} from './components/auth/register/register.component'
+import {HomepageComponent} from './pages/home/homepage.component'
 
 export const appRoutes: Route[] = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: '', redirectTo: '/register', pathMatch: 'full'}
+  {path: '', component: HomepageComponent}
 ]
