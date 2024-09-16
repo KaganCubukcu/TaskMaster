380b4ec2d29c94492cc6ef886f85c0b2d6530636
kagan <kagancancubukcu@gmail.com>
2024-09-16 18:34:29 +0300

    Protected login and register routes via Auth Guard

--- todoApp-Client/src/app/app.routes.ts
+++ todoApp-Client/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import {Route} from '@angular/router'
 import {LoginComponent} from './components/auth/login/login.component'
 import {RegisterComponent} from './components/auth/register/register.component'
 import {HomepageComponent} from './pages/home/homepage.component'
+import {AuthGuard} from './guards/auth.guard'
 
 export const appRoutes: Route[] = [
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent, canActivate: [AuthGuard]},
+  {path: 'register', component: RegisterComponent, canActivate: [AuthGuard]},
   {path: '', component: HomepageComponent}
 ]
